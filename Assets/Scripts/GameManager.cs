4b3fe7629b1e97792f2e91a7d52de9dd521b4a94
Brnmg <bmg.gerardo@gmail.com>
2022-08-30 22:07:53 -0300

    Movement system, start/end game and UI components

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class GameManager : MonoBehaviour
 {
+    public PlayerController player; 
+    public TMP_Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
     private int score;
+
+    public void Awake()
+    {
+        Pause();
+        Application.targetFrameRate = 60;
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        SoulFragments[] soulFragments = FindObjectsOfType<SoulFragments>();
+
+        for(int i = 0; i < soulFragments.Length; i++)
+        {
+            Destroy(soulFragments[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
 
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,34 +6,31 @@ public class PlayerController : MonoBehaviour
 {
     public Sprite shipIdle, shipDown, shipUp;
     private Rigidbody2D rb2D;
-    public Joystick joystick;
 
     // Player's physics 
-    private float moveSpeed;
-    private float moveVertical;
+    public float strength = .1f;
 
     private void Start()
     {
         // Get the player's (gameObject) RigidBody2D values
         rb2D = gameObject.GetComponent<Rigidbody2D>();
-        moveSpeed = .05f;
     }
 
     void Update()
     {
+
     }
 
     private void FixedUpdate()
     {
-        if (moveVertical > 0.1f)
+        if (Input.GetKey(KeyCode.Space))
         {
             GetComponent<SpriteRenderer>().sprite = shipUp;
-            rb2D.AddForce(new Vector2(0, moveVertical * moveSpeed), ForceMode2D.Impulse);
+            rb2D.AddForce(new Vector2(0, strength), ForceMode2D.Impulse);
         }
-        if (moveVertical < -0.1f)
+        else
         {
             GetComponent<SpriteRenderer>().sprite = shipDown;
-            rb2D.AddForce(new Vector2(0, moveVertical * moveSpeed), ForceMode2D.Impulse);
         }
     }
 
@@ -42,6 +39,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.tag == "Scoring")
         {
             FindObjectOfType<GameManager>().IncreaseScore();
+            Destroy(collision.gameObject);
         }        
     }
 }
